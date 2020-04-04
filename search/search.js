24ec612f68f1e2819eb84a9289d93a5c8c20edf0
immy458 <59685135+immy458@users.noreply.github.com>
2020-04-04 15:48:08 +0530

    Update search.js

--- search/search.js
+++ search/search.js
@@ -1,64 +1,80 @@
 
 function check(){
-   var videoId;
-        var videoTitle ;
-           var i=0;
-const videoList = document.getElementById('results');
- var x = document.forms["form"]["search"].value;
+var videoId;
+var videoTitle ;
+
+var x = document.forms["form"]["search"].value;
   if (x == "" || x==" ") {
       document.getElementById("results").style.display = "none";
   }
-  else
-  {
-        
-       var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-            maxResults: 6,
-            order: "relevance",
-            publishedAfter: "2011-01-01T00:00:00Z"
-       }); 
+else
+{
+  const key = 'AIzaSyC-s7qt1-D8FyTGRMebBoLGMC1eF25tly8';
+  var URL = 'https://www.googleapis.com/youtube/v3/search';   
+  const videoList = document.getElementById('results');
+  //youtube API sees all these info, so that it knows what kind of information you want to retrieve
+ 
+  const options = {     //javascript object which contains properties and values
+        key: key,
+        part: "snippet",
+        type: "video",
+        q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+        maxResults: 6,
+        order: "relevance",
+        publishedAfter: "2011-01-01T00:00:00Z"
+  };
 
-   
-      
-       request.execute(function(response) {
-              const listItems = response.result.items;
-        if (listItems) 
-        {
-            let output = '<ul style="list-style-type: none;">';
+  document.getElementById("results").style.display = "block";    
+
+  let loadvideolist = function () {
+
+    var esc = encodeURIComponent;
+    URL1 = Object.keys(options)     //returns arrays of keys
+      .map(k => esc(k) + '=' + esc(options[k]))
+      .join('&');
+    URL=URL+"?"+URL1;
+    alert(URL);
+
+  //console.log(URL);
+
+    //use fetch to get HTTP request
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+    //    console.log("dataaaaaaaaaaaaa");
+        console.log(data);
+          const listItems =data.items;
+          if (listItems) 
+          {
+              let output = '<ul style="list-style-type: none;">';
+              
+              listItems.forEach(item => 
+              {
+                    videoId = item.id.videoId;
             
-            listItems.forEach(item => 
-            {
-                
-                 videoId = item.id.videoId;
-                 videoTitle = item.snippet.title;
-                output += `
-                    <li style="border:1px solid black"; >
-                    <a href="//www.youtube.com/embed/${videoId}" style="color:black;cursor:pointer;">
-                    <img src="http://i3.ytimg.com/vi/${videoId}/hqdefault.jpg" height="120" id=${videoId}/>
-                  ${videoTitle}
-                    </li>
-                    </a>
-                    <br>
-                `;
-            });
-            output += '</ul>'; 
-            // Output list
-            videoList.innerHTML = output;
-        }
+                   videoTitle = item.snippet.title;
+             
+                  output += `
+                      <li style="border:1px solid black"; >
+                      <a href="//www.youtube.com/embed/${videoId}" style="color:black;cursor:pointer;">
+                      <img src="http://i3.ytimg.com/vi/${videoId}/hqdefault.jpg" height="120" id=${videoId}/>
+                    ${videoTitle}
+                      </li>
+                      </a>
+                      <br>
+                  `;
+              });
+              output += '</ul>'; 
+              // Output list
+              videoList.innerHTML = output;
+          }
 
-     }, function(err) { console.error("Execute error", err); });
 
-        document.getElementById("results").style.display = "block";    
+      })
+      .catch(err => console.log(err))
+
   }
+  loadvideolist();
 }
 
-function init() {
-   
-    gapi.client.setApiKey("AIzaSyC-s7qt1-D8FyTGRMebBoLGMC1eF25tly8");
-    gapi.client.load("youtube", "v3", function() {  
-     console.log("done");
-    });
 }
-
